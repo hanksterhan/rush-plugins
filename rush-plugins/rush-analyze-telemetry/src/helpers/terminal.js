b2231a5df27f73468ebf565043b163062dbe547f
Henry H <henryh@adobe.com>
2022-08-31 18:14:56 -0400

    rush analyze-telemetry is now recognized on the command line

---
+++ rush-plugins/rush-analyze-telemetry/src/helpers/terminal.js
@@ -0,0 +1,5 @@
+"use strict";
+exports.__esModule = true;
+exports.terminal = void 0;
+var node_core_library_1 = require("@rushstack/node-core-library");
+exports.terminal = new node_core_library_1.Terminal(new node_core_library_1.ConsoleTerminalProvider());
--- rush-plugins/rush-analyze-telemetry/src/index.ts
+++ rush-plugins/rush-analyze-telemetry/src/index.ts
@@ -1,13 +1,7 @@
 #!/usr/bin/env node
 
-import os from 'os';
-import path from 'path';
-
-import { loadRushConfiguration } from "./helpers/loadRushConfiguration";
 import { terminal } from "./helpers/terminal";
 
-import type { RushConfiguration } from "@rushstack/rush-sdk";
-import { JsonFile } from '@rushstack/node-core-library';
 
 
 // eslint-disable-next-line @typescript-eslint/no-floating-promises
@@ -15,7 +9,6 @@ main();
 
 async function main(): Promise<void> {
   try {
-    const rushConfig: RushConfiguration = loadRushConfiguration();
     
   } catch (error: any) {
     if (error.message) {
